57f89ad25051f9342bf89adbc5ff346bdd6d45ed
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-17 20:20:46 -0400

    feat: Redirect user to the requested Url

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ShortUrl = require("./models/shortUrl");
 const app = express();
 
-mongoose.connect("mongodb://localhost/urlShortener", {
+mongoose.connect("mongodb://127.0.0.1/urlShortener", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -19,6 +19,17 @@ app.get("/", async (req, res) => {
 app.post("/shortUrls", async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl });
   res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
 });
 
 app.listen(process.env.PORT || 5000);
